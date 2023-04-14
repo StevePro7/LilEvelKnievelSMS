123056e25ae0ec4a84b19b8392f742c90a3a079d
StevePro7 <steveprozune@hotmail.com>
2023-04-14 16:36:38 +0100

    no message

--- 01_01/dev/banks/bank10.h
+++ 01_01/dev/banks/bank10.h
@@ -7,26 +7,26 @@ extern const unsigned char level_0402_txt[];
 #define				level_0402_txt_bank 10
 
 extern const unsigned char level_0403_txt[];
-#define				level_0403_txt_size 2048
+#define				level_0403_txt_size 256
 #define				level_0403_txt_bank 10
 
 extern const unsigned char level_0404_txt[];
-#define				level_0404_txt_size 2048
+#define				level_0404_txt_size 256
 #define				level_0404_txt_bank 10
 
 extern const unsigned char level_0405_txt[];
-#define				level_0405_txt_size 2048
+#define				level_0405_txt_size 256
 #define				level_0405_txt_bank 10
 
 extern const unsigned char level_0406_txt[];
-#define				level_0406_txt_size 2048
+#define				level_0406_txt_size 256
 #define				level_0406_txt_bank 10
 
 extern const unsigned char level_0407_txt[];
-#define				level_0407_txt_size 2048
+#define				level_0407_txt_size 256
 #define				level_0407_txt_bank 10
 
 extern const unsigned char level_0408_txt[];
-#define				level_0408_txt_size 2048
+#define				level_0408_txt_size 256
 #define				level_0408_txt_bank 10
 
